d4dde13edc72694081cbabe545868a7683935b57
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2015-10-16 18:23:11 -0400

    updates for hw2/prob1

--- hw2/logistic_regression.py
+++ hw2/logistic_regression.py
@@ -1,36 +1,43 @@
 __author__ = 'manuelli'
 import numpy as np
-
+import matplotlib.pyplot as plt
+import sys
+sys.path.append("../hw1")
+from gradient_descent import GradientDescent as gd
 
 class LogisticRegression:
 
     def __init__(self, x, y):
-        self.N = np.size(self.y)
+        self.N = np.size(y)
         self.y = np.reshape(y,(self.N,))
         self.x = x
         self.d = np.shape(x)[1] # this is the size of w
         # pad with ones at the beginning
-        self.x_full = np.zeros((N,d+1))
-        self.x_full[:,0] = np.zeros(N)
+        self.x_full = np.zeros((self.N,self.d+1))
+        self.x_full[:,0] = np.ones(self.N)
         self.x_full[:,1:] = self.x
 
 
-
-    def NLL(self,w_full):
+    def splitW(self,w_full):
         w_0 = w_full[0]
         w = w_full[1:]
-        exponent = -1.0*numpy.multiply(self.y, np.dot(self.x, w) + w_0)
+        return (w_0,w)
+
+    def NLL(self,w_full):
+        (w_0, w) = self.splitW(w_full)
+        exponent = -1.0*np.multiply(self.y, np.dot(self.x, w) + w_0)
         nll = np.sum(np.log(1.0 + np.exp(exponent)))
 
         return nll
 
     def NLL_Reg(self, w_full, lam):
         w = w_full[1:]
-        return self.NLL(w_full) + lam*np.norm(w)
+        return self.NLL(w_full) + lam*np.linalg.norm(w)
 
     # computes the gradient of NLL, not the regularization term though
     def NLL_grad(self, w_full):
-        exponent = -1.0*numpy.multiply(self.y, np.dot(self.x, w) + w_0)
+        (w_0, w) = self.splitW(w_full)
+        exponent = -1.0*np.multiply(self.y, np.dot(self.x, w) + w_0)
         t = np.log(1.0 + np.exp(exponent))
         t_inv = np.power(t,-1) # this is N x 1
         a = -1.0*np.multiply(self.y[:,None], self.x_full) # this is -y x_full = N x (d+1)
@@ -46,7 +53,6 @@ class LogisticRegression:
         return nllGrad + regGrad
 
 
-
     def predict(self, w_full, x):
         w_0 = w_full[0]
         w = w[1:]
@@ -56,11 +62,6 @@ class LogisticRegression:
             return 1
         else:
             return 0
-
-    def splitW(self,w_full):
-        w_0 = w_full[0]
-        w = w_full[1:]
-        return (w_0,w)
 
     # compute the classification error rate for a given dataset
     def classificationErrorRate(self, w_full):
@@ -75,12 +76,23 @@ class LogisticRegression:
 
         return missclassifiedRate, missclasified
 
+    def plotData(self):
+        idx_pos = np.where(self.y > 0)
+        idx_neg = np.where(self.y < 0)
+        plt.scatter(self.x[idx_pos,0], self.x[idx_pos,1], color='b', marker='o', facecolors='none')
+        plt.scatter(self.x[idx_neg,0], self.x[idx_neg,1], color='r', marker='o', facecolors='none')
+
+    def constructGradientDescentObject(self, lam=0):
+        f = lambda w_full: self.NLL_Reg(w_full, lam)
+        grad = lambda w_full: self.NLL_Reg_grad(w_full, lam)
+        gradDescent = gd(f,grad=grad)
+
 
 
     @staticmethod
     def fromFile(file, type="train"):
-        filename = "/hw2_resources/data/" + file + "_" + type + ".csv"
-        train = loadtxt(filename)
+        filename = "hw2_resources/data/" + file + "_" + type + ".csv"
+        train = np.loadtxt(filename)
         X = np.array(train[:,0:2])
         Y = np.array(train[:,2:3])
         lr = LogisticRegression(X,Y)
