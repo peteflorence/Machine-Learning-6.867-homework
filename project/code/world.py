50106eb98dc47abc91a3ea690d25b30d964e141e
peteflorence <pete.florence@gmail.com>
2015-11-18 01:13:01 -0500

    big world

--- project/code/world.py
+++ project/code/world.py
@@ -2,6 +2,8 @@ import ddapp.vtkAll as vtk
 import ddapp.visualization as vis
 from ddapp.debugVis import DebugData
 
+import numpy as np
+
 from PythonQt import QtCore, QtGui
 
 class World(object):
@@ -12,10 +14,35 @@ class World(object):
 
     @staticmethod
     def buildSimpleWorld():
-
         d = DebugData()
         d.addLine((2,-1,0), (2,1,0), radius=0.1)
         d.addLine((2,-1,0), (1,-2,0), radius=0.1)
+        obj = vis.showPolyData(d.getPolyData(), 'world')
+        return obj
+
+    @staticmethod
+    def buildBigWorld():
+        d = DebugData()
+
+        worldXmin = -20
+        worldXmax = 100
+
+        worldYmin = -50
+        worldYmax = 50
+
+        numObs = 200
+        obsLength = 4
+
+        for i in xrange(numObs):
+            firstX = worldXmin + np.random.rand()*(worldXmax-worldXmin)
+            firstY = worldYmin + np.random.rand()*(worldYmax-worldYmin)
+            firstEndpt = (firstX,firstY,0)
+            
+            randTheta = np.random.rand() * 2.0*np.pi
+            secondEndpt = (firstX+np.cos(randTheta), firstY+np.sin(randTheta), 0)
+
+            d.addLine(firstEndpt, secondEndpt, radius=0.1)
+
         obj = vis.showPolyData(d.getPolyData(), 'world')
         return obj
 
