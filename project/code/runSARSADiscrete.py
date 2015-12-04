eedff61e50cbe6284c72012889e8dab78f6da656
Lucas Manuelli <lucas.t.manuelli@gmail.com>
2015-12-04 16:13:50 -0500

    WORKING, runSARSADiscrete.py really works

--- project/code/runSARSADiscrete.py
+++ project/code/runSARSADiscrete.py
@@ -4,25 +4,19 @@ from simulator import Simulator
 sim = Simulator(autoInitialize=False, verbose=False)
 
 sim.sarsaType = "discrete"
-sim.Sarsa_numInnerBins = 5
-sim.Sarsa_numOuterBins = 4
-sim.Sensor_rayLength = 10
+sim.options['SARSA']['lam'] = 0.7
+sim.options['SARSA']['numInnerBins'] = 5
+sim.options['SARSA']['numOuterBins'] = 4
+sim.options['SARSA']['binCutoff'] = 0.5
+sim.options['Sensor']['rayLength'] = 10
+sim.options['Sensor']['numRays'] = 20
 
-
-sim.randomSeed = 11
-sim.randomizeControl       = True
-sim.percentObsDensity      = 3.5
-sim.nonRandomWorld         = True
-sim.circleRadius           = 2.5
-sim.worldScale             = 1
-sim.supervisedTrainingTime = 3000
-sim.learningRandomTime = 4000
-sim.learningEvalTime = 500
-sim.defaultControllerTime = 500
 sim.options['SARSA']['burnInTime'] = sim.supervisedTrainingTime/2.0
 sim.options['Reward']['actionCost'] = 0.2
 sim.options['Reward']['raycastCost'] = 40.0
 # sim.options['Reward']['collisionPenalty'] = 200
+
+
 
 
 
